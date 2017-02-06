4a94536e7eaeb1672cf85eac204bd3eff8986a70
Minwoo <pmw9027@naver.com>
2017-02-06 14:50:57 +0900

    test

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var client = require('./routes/client');
+
 
 var app = express();
 
@@ -24,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
+app.use('/client',client);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
