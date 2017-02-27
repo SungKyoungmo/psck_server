f5c44a14e3d5c799a93c6df0bcad1dfa5015f915
Sung Kyungmo <skm280@naver.com>
2017-02-27 23:54:29 +0900

    add deviceinfo.js create device_info collection update cpu, memory data

--- routes/device/info.js
+++ routes/device/info.js
@@ -2,17 +2,42 @@
  * Created by Minwoo on 2017. 2. 8..
  */
 
-exports.post = function(req, res){
-    //console.log(req.body);
-    console.log("Request handler random was called.");
+exports.post = function (req, res) {
 
+    var Device = require("../../models/deviceinfo");
     res.writeHead(200, {"Content-Type": "application/json"});
-    var otherArray = ["item1", "item2"];
-    var otherObject = { item1: "item1val", item2: "item2val" };
-    var json = JSON.stringify({
-        anObject: otherObject,
-        anArray: otherArray,
-        another: "item"
-    });
+    var json = JSON.stringify(req.body);
+
+
+    try {
+        Device.findOne({u_id: req.body.u_id}, function (err, device) {
+            if (err) return res.status(500).json({
+                error: err
+            });
+
+            var device_info = new Device(req.body);
+
+            if (device == null) {
+                device_info.save(function (err, createdTodoObject) {
+                    if (err) return res.status(500).json({
+                        error: err
+                    });
+                });
+            }
+            else {
+                Device.update(
+                    {u_id: req.body.u_id},
+                    {$set: {d_cpu_per: req.body.d_cpu_per, d_mem_per: req.body.d_mem_per}},
+                    function (err, result) {
+                        if (err) throw err;
+                    })
+            }
+        })
+    }
+    catch
+        (e) {
+        console.log(e)
+    }
+
     res.end(json);
 };
